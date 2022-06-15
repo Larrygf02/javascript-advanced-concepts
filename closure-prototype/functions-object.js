5d33e02be7336d9b104e8deb095f4034de4774bb
Larrygf02 <raulgf_02@hotmail.com>
2022-06-14 23:51:02 -0500

    function are objects

---
+++ closure-prototype/functions-object.js
@@ -0,0 +1,7 @@
+// ojects {} => Array[] or Function()
+function wohoo() {
+    console.log('wohoo')
+}
+
+wohoo.yell = 'ahaa' // como las funciones son objeto, puedo agregar una variable al objeto
+
