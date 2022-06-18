50de5b485474c0dac21f92cceba031f0cd68341b
Larrygf02 <raulgf_02@hotmail.com>
2022-06-18 16:25:47 -0500

    prototype herencia

--- closure-prototype/prototype.js
+++ closure-prototype/prototype.js
@@ -1,3 +1,24 @@
-const array = []
+let dragon = {
+    name: 'Tania',
+    fire: true,
+    fight() {
+        return 5
+    },
+    sing() {
+        if (this.fire) {
+            return `I am ${this.name}, the breather of fire`
+        }
+    }
+}
 
-console.log(array.__proto__)
+let lizard = {
+    name: 'kiki',
+    fight() {
+        return 1
+    }
+}
+
+// const singLizard = dragon.sing.bind(lizard)
+// console.log(singLizard())
+lizard.__proto__ = dragon // funciona como herencia "lizard" hereda todos los metodos de dragon
+console.log(lizard.fight())
