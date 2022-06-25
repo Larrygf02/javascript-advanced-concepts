3a015831984cc2bfe9b8fac2379b7e77eaa7d9d8
Larrygf02 <raulgf_02@hotmail.com>
2022-06-24 21:34:23 -0500

    scope

---
+++ intermediate/scope.js
@@ -0,0 +1,21 @@
+var fun = 4
+
+function funfest() {
+    var fun = 10
+    console.log(fun)
+}
+
+function funerFunction() {
+    var fun = 8
+    console.log(fun)
+}
+
+function funestFunction() {
+    var fun = 19
+    console.log(fun)
+}
+
+console.log(fun)
+funfest()
+funerFunction()
+funestFunction()
