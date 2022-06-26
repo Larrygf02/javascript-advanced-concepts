2c18e2e4dddd7caa7fa51ad3cd78ddba462343de
Larrygf02 <raulgf_02@hotmail.com>
2022-06-25 20:52:12 -0500

    es8

---
+++ intermediate/es7.js
@@ -0,0 +1,17 @@
+// es7
+console.log('Helloo'.includes('o'))
+
+const numbers = [3,6,7]
+console.log(numbers.includes(6))
+
+// es8
+console.log('Hello'.padStart(10))
+console.log('Hello'.padEnd(10))
+
+let obj = {
+    'username': 'josue',
+    'lastname': 'carbajo',
+    'age': 24
+}
+console.log(Object.keys(obj))
+console.log(Object.values(obj))
