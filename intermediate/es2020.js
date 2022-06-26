bed43908bf9664f886871454ca73d178a46f62f6
Larrygf02 <raulgf_02@hotmail.com>
2022-06-25 23:35:11 -0500

    es2020

---
+++ intermediate/es2020.js
@@ -0,0 +1,12 @@
+// BigInt
+const hugeHex = BigInt("0x1fffffffffffff")
+
+//
+let will_pokemon = {
+    pikachu: {
+        specie: 'Mouse pokemon',
+        height: 0.4,
+        weight: 8
+    }
+}
+console.log(will_pokemon.pikachu?.weight)
